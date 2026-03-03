6c983a39a2f045efd9b0fbedce48dc4eb2229b3c
Juan Esteban M <imaes.st@gmail.com>
2026-03-03 09:57:22 -0500

    Validation with Express OpenApi Validator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const OpenApiValidator = require('express-openapi-validator');
+
 const app = express();
 const port = 3000;
 
 const swaggerDocument = YAML.load('./openapi.yaml');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: swaggerDocument,
+    validateRequests: true,
+    validateResponses: true,
+    ignorePaths: /.*\/docs.*/, // Ignore the docs path
+  })
+);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({  
+    message: err.message,
+    errors: err.errors,
+  });
+});
 
 app.get('/hello', (req, res) => {
   res.json({'message': 'Hello, World!'});
