5fc3d80aa6ebd7392f29ba4efe1569b7e2a7b6f0
Juan Esteban M <imaes.st@gmail.com>
2026-03-03 10:22:56 -0500

    Validation Endpoint /users

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const port = 3000;
 const swaggerDocument = YAML.load('./openapi.yaml');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
 
 app.use(
   OpenApiValidator.middleware({
@@ -28,6 +29,17 @@ app.use((err, req, res, next) => {
 
 app.get('/hello', (req, res) => {
   res.json({'message': 'Hello, World!'});
+});
+
+app.post('/users', (req, res) => {
+    const { name, age, email } = req.body;
+    const newUser = {
+        id: Date.now().toString(),
+        name,
+        age,
+        email
+    }
+  res.status(201).json(newUser);
 });
 
 app.listen(port, () => {
